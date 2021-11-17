aa22f8e3db1b36bf7c33853914a516008071112d
“Brijesh” <brijeshkulal5@gmail.com>
2021-11-17 21:54:18 +0530

    update the salary of employee using prepared statement

--- src/com/jdbc/employeeroll/Employee_PayRollJDBC.java
+++ src/com/jdbc/employeeroll/Employee_PayRollJDBC.java
@@ -1,15 +1,15 @@
 package com.jdbc.employeeroll;
 import java.sql.*;
-import java.util.Enumeration;
 
 public class Employee_PayRollJDBC {
+	
 	public static void main(String[] args) throws Exception {
-		String query= "update payroll_service.employee_payroll set salary =3000000 where empName ='Shreya'";
+		String query= "update payroll_service.employee_payroll set salary =30000 where empName ='Shreya'";
 		Class.forName("com.mysql.jdbc.Driver");
 		System.out.println("Driver loaded");
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306?user = root & password = Brijesh@534");
 		System.out.println("connection established");
-		Statement statement = con.createStatement();
+		PreparedStatement statement = con.prepareStatement(query);
 		System.out.println("platform created");
 		int count = statement.executeUpdate(query);
 		System.out.println(count + " row/s affected");
