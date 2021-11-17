dad04807b0845850ee34a82c2de29c57437946cb
“Brijesh” <brijeshkulal5@gmail.com>
2021-11-17 21:43:20 +0530

    update the salary of employee

--- src/com/jdbc/employeeroll/Employee_PayRollJDBC.java
+++ src/com/jdbc/employeeroll/Employee_PayRollJDBC.java
@@ -3,26 +3,17 @@ import java.sql.*;
 import java.util.Enumeration;
 
 public class Employee_PayRollJDBC {
-	public static void main(String[] args) {
-	try {
+	public static void main(String[] args) throws Exception {
+		String query= "update payroll_service.employee_payroll set salary =3000000 where empName ='Shreya'";
 		Class.forName("com.mysql.jdbc.Driver");
 		System.out.println("Driver loaded");
-	} catch (ClassNotFoundException e) {
-		throw new IllegalStateException("cannot find the driver" + e);
-	}
-	try {
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306?user = root & password = Brijesh@534");
 		System.out.println("connection established");
-	} catch (SQLException e) {
-		e.printStackTrace();
+		Statement statement = con.createStatement();
+		System.out.println("platform created");
+		int count = statement.executeUpdate(query);
+		System.out.println(count + " row/s affected");
+		statement.close();
+		con.close();
 	}
-	}
-	
-	private static void listDrivers() {
-		  Enumeration<java.sql.Driver> driverList = DriverManager.getDrivers();
-		  while (driverList.hasMoreElements()) {
-			  Driver driverClass =(Driver) driverList.nextElement();
-			  System.out.println("  "+driverClass.getClass().getName());
-		  }
-	  } 
 }
