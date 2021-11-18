8e5c8a9b0f9674d1a1e71d36fe22b8a32830877d
“Brijesh” <brijeshkulal5@gmail.com>
2021-11-18 22:07:07 +0530

    calculated average,sum,count,min,max

--- src/com/jdbc/employeeroll/Employee_PayRollJDBC.java
+++ src/com/jdbc/employeeroll/Employee_PayRollJDBC.java
@@ -24,38 +24,37 @@ public class Employee_PayRollJDBC {
 			ResultSet result1 = statement.executeQuery(query1);
 			if(result1.next()){
 				System.out.println("Salary =" +result1.getString("SUM(Salary)"));
-            }
+				}
 			
 			ResultSet result2 = statement.executeQuery(query2);
 			if(result2.next()){
 				System.out.println("Salary =" +result2.getString("SUM(Salary)"));
-            }
+				}
 			
 			ResultSet result3 = statement.executeQuery(query3);
 			if(result3.next()){
 				System.out.println("Salary =" +result3.getString("AVG(Salary)"));
-            }
+				}
 			
 			ResultSet result4 = statement.executeQuery(query4);
 			if(result4.next()){
 				System.out.println("Salary =" +result4.getString("AVG(Salary)"));
-            }
+				}
 			
 			ResultSet result5 = statement.executeQuery(query5);
 			if(result5.next()){
 				System.out.println("Salary =" +result5.getString("MIN(Salary)"));
-            }
+				}
 			
 			ResultSet result6 = statement.executeQuery(query6);
 			if(result6.next()){
-				System.out.println("Salary =" +result6.getString("MAX(Salary)")); ;
-            }
+				System.out.println("Salary =" +result6.getString("MAX(Salary)"));
+				}
 			
 			ResultSet result7 = statement.executeQuery(query7);
 			if(result7.next()){
-				System.out.println("Salary =" +result7.getString("COUNT(Salary)")); ;
-            }
-			
+				System.out.println("Salary =" +result7.getString("COUNT(Salary)"));
+			}
 		}
 		catch(ClassNotFoundException | SQLException ex){
 			 ex.printStackTrace();
