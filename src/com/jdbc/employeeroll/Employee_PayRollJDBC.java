7da07678fb767da539c0d6558f2589c464ad908f
“Brijesh” <brijeshkulal5@gmail.com>
2021-11-18 21:15:42 +0530

    Retrived payroll data by name using preparedstatement

--- src/com/jdbc/employeeroll/Employee_PayRollJDBC.java
+++ src/com/jdbc/employeeroll/Employee_PayRollJDBC.java
@@ -1,12 +1,17 @@
 package com.jdbc.employeeroll;
 import java.sql.*;
+import java.util.Scanner;
 
 public class Employee_PayRollJDBC {
 	
 	public static void main(String[] args)  {
 		Connection connection = null;
         PreparedStatement statement = null;
-		String query= "update payroll_service.employee_payroll where empName ='Dashwath'";
+		String query= "select * from payroll_service.employee_payroll where empName = ?";
+		Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Employee Name to fetch Employee payroll details");
+        String Name = sc.next();
+        sc.close();
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			System.out.println("Driver loaded");
@@ -14,9 +19,19 @@ public class Employee_PayRollJDBC {
 			System.out.println("connection established");
 			statement = con.prepareStatement(query);
 			System.out.println("platform created");
-			statement.setFloat(3, (float) 3000000.00);
-			int count = statement.executeUpdate(query);
-			System.out.println(count + " row/s affected");
+			statement.setString(1, Name);
+            ResultSet resultSet = statement.executeQuery();
+            System.out.println("data fetched");
+            if(resultSet.next()){
+            	String empId = resultSet.getString(1);
+                String empName = resultSet.getString(2);
+                String salary = resultSet.getString(3);
+                Date startDate = resultSet.getDate(4);
+                System.out.println("name:" + empId+ " Employee Name : "+ empName+ " Salary: "+ salary+" Start date: "+startDate);
+             }
+            else{
+                System.out.println("no data found : "+ Name);
+            }
 		}
 		catch(ClassNotFoundException | SQLException ex){
 			((Throwable) ex).printStackTrace();
